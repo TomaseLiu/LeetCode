0d6270bd3f5f7b86635b957cefe5d93d2c954366
Tomase Liu <xiangzi777@sjtu.edu.cn>
2014-06-23 22:25:59 +0800

    first change
    Add a main file for dealing with BreakWord

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
+using namespace boost;
+
+bool wordBreak(string s, unordered_set<string> &dict);
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+bool wordBreak(string s, unordered_set<string> &dict)
+{
+    return false;
+}
